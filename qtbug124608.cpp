4f546086450cda06860bd5b44b9e47fdfdb6f629
Janez Žemva <janezz55@gmail.com>
2024-04-22 17:27:11 +0200

    some fixes

---
+++ qtbug124608.cpp
@@ -0,0 +1,50 @@
+#include <QFile>
+#include <QPainter>
+
+#include "qtnanosvg.hpp"
+
+#include "nanosvg/src/nanosvg.h"
+
+#include "svgimageprovider.hpp"
+
+//////////////////////////////////////////////////////////////////////////////
+SVGImageProvider::SVGImageProvider():
+  QQuickImageProvider(QQmlImageProviderBase::Pixmap)
+{
+}
+
+//////////////////////////////////////////////////////////////////////////////
+QPixmap SVGImageProvider::requestPixmap(QString const& id, QSize* const sz,
+  QSize const& rs)
+{
+  QPixmap pm(*sz = rs);
+
+  if (!pm.isNull())
+  {
+    uchar* dat;
+
+    if (QFile f(id); f.open(QIODevice::ReadOnly) &&
+      (dat = f.map({}, f.size(), QFileDevice::MapPrivateOption)))
+    {
+      dat[f.size() - 1] = {};
+
+      if (auto const nsi(nsvgParse(
+        reinterpret_cast<char*>(dat), "px", 96)); nsi)
+      {
+        pm.fill(Qt::transparent);
+
+        {
+          QPainter p(&pm);
+          p.setRenderHint(QPainter::Antialiasing);
+
+          drawSVGImage(&p, nsi, rs.width(), rs.height());
+        }
+
+        nsvgDelete(nsi);
+        f.unmap(dat);
+      }
+    }
+  }
+
+  return pm;
+}
