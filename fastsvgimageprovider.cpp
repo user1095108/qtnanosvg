cda8a26fbbd74830bd0f1b2571f0fe53a6588335
Janez Žemva <janezz55@gmail.com>
2024-06-29 20:49:46 +0200

    some fixes

--- fastsvgimageprovider.cpp
+++ fastsvgimageprovider.cpp
@@ -26,7 +26,7 @@ QPixmap SVGImageProvider::requestPixmap(QString const& id, QSize* const sz,
 {
   QPixmap pm(*sz = rs);
 
-  while (!pm.isNull())
+  do if(!pm.isNull())
   {
     char* dat;
 
@@ -51,9 +51,7 @@ QPixmap SVGImageProvider::requestPixmap(QString const& id, QSize* const sz,
 
       nsvgDelete(nsi);
     }
-
-    break;
-  }
+  } while (false);
 
   return pm;
 }
