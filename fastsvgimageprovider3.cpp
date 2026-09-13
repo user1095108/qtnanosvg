634a7f35af774e4cc67180d803c91976920f53ea
Janez Žemva <janezz55@gmail.com>
2026-09-13 13:51:46 +0200

    some fixes

---
+++ fastsvgimageprovider3.cpp
@@ -0,0 +1,50 @@
+#include <QFile>
+#include <QPainter>
+#include <QVarLengthArray>
+
+#include "nanosvg/src/nanosvg.h"
+#include "qtnanosvg.hpp"
+
+#include "svgimageprovider.hpp"
+
+//////////////////////////////////////////////////////////////////////////////
+SVGImageProvider::SVGImageProvider():
+  QQuickImageProvider(QQmlImageProviderBase::Pixmap)
+{
+}
+
+//////////////////////////////////////////////////////////////////////////////
+QPixmap SVGImageProvider::requestPixmap(QString const& id, QSize* const sz,
+  QSize const& rs)
+{
+  QPixmap pm(*sz = rs);
+
+  if (!pm.isNull())
+  {
+    if (QFile f(id); f.open(QIODevice::ReadOnly))
+    {
+      auto const fsz(f.size());
+
+      if (QVarLengthArray<char> dat(fsz + 1); fsz == f.read(dat.data(), fsz))
+      {
+        dat[fsz] = {};
+
+        if (auto const nsi(nsvgParse(dat.data(), "px", 96)); nsi)
+        {
+          pm.fill(Qt::transparent);
+
+          {
+            QPainter p(&pm);
+            p.setRenderHint(QPainter::Antialiasing);
+
+            drawSVGImage(&p, nsi, rs.width(), rs.height());
+          }
+
+          nsvgDelete(nsi);
+        }
+      }
+    }
+  }
+
+  return pm;
+}
